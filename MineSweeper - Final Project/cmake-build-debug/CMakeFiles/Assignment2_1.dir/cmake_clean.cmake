25932be5e73c38b8276a381cf16a6131b1b2428a
BahaaShammary <32821070+BahaaShammary@users.noreply.github.com>
2019-04-27 12:35:19 +0100

    Minesweeper Game - updated

---
+++ MineSweeper - Final Project/cmake-build-debug/CMakeFiles/Assignment2_1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Assignment2_1.dir/main.c.obj"
+  "CMakeFiles/Assignment2_1.dir/game_functions.c.obj"
+  "Assignment2_1.pdb"
+  "Assignment2_1.exe"
+  "Assignment2_1.exe.manifest"
+  "libAssignment2_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Assignment2_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- MineSweeper - Final Project/main.c
+++ MineSweeper - Final Project/main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "game_functions.h"
+#include <string.h>
+
 //
 // Created by Bahaulddin
 //
-int main() {
+int main(int argc, char* argv[]) {
     int bomb_location_info[4][4] =     { 1,1,1,0,
                                          1,0,2,1,
                                          1,1,2,0,
@@ -17,19 +19,62 @@ int main() {
 
     int of_bombs = 2;
     struct locations* pbombs = (struct locations * ) malloc(sizeof(struct locations)  * 2) ;
+//    printf("Number of bombs %d\n", of_bombs);
+//    printf("Number of argc %d\n", argc);
+//    printf("clues? %s 1\n", argv[1]);
 
-    pbombs[0].x = 1;
-    pbombs[0].y  = 1;
-    pbombs[0].found = false;
+    if (argc == 2) {
+        printf("Please provide the command line arguments");
+    }
+    int i;
+    for (i=0;i<argc;i++) {
+        printf("\n%s ", argv[i]);
+    }
+    printf("\nNumber of bombs %d", of_bombs);
+    int compare = strcmp(argv[1], "clues");
+    int compare1 = strcmp(argv[1], "noclues");
 
-    pbombs[1].x = 2;
-    pbombs[1].y  = 3;
-    pbombs[1].found = false;
+    int k;
+    int randomNum1;
+    int randomNum2;
+    for (k=0;k<2;k++) {
+        randomNum1 = rand() % 3 + 1;
+        randomNum2 = rand() % 3 + 1;
+        printf("\nrandom1: %d random2: %d\n", randomNum1, randomNum2);
+        known_location_info[randomNum1][randomNum2] = bomb_location_info[randomNum1][randomNum2];
+        known_location_info[randomNum1][randomNum2] = bomb_location_info[randomNum1][randomNum2];
+    }
 
-    int size = DIM;
 
+    if (compare ==0 ) {
+        printf("\nCLUES\n");
 
-    start_game(pbombs, bomb_location_info, DIM, known_location_info, of_bombs);
+        pbombs[0].x = 1;
+        pbombs[0].y  = 1;
+        pbombs[0].found = false;
+
+        pbombs[1].x = 2;
+        pbombs[1].y  = 3;
+        pbombs[1].found = false;
+
+        int size = DIM;
+
+        start_game(pbombs, bomb_location_info, DIM, known_location_info, of_bombs);
+    }
+    else if (compare1 == 0) {
+        printf("\nNOCLUES\n");
+        pbombs[0].x = 1;
+        pbombs[0].y  = 1;
+        pbombs[0].found = false;
+
+        pbombs[1].x = 2;
+        pbombs[1].y  = 3;
+        pbombs[1].found = false;
+
+        int size = DIM;
+
+        start_game(pbombs, bomb_location_info, DIM, known_location_info, of_bombs);
+    }
 
     free(pbombs);
 
